b990b29c9c69302df4a448a4fa636f67dee67119
liuwenbo <eryuehao@gmail.com>
2019-12-17 14:06:02 +0800

    删除FLutter module

--- app/src/main/java/com/workbook/liuwb/workbook/flutter/TestFlutterActivity.kt
+++ app/src/main/java/com/workbook/liuwb/workbook/flutter/TestFlutterActivity.kt
@@ -1,12 +1,8 @@
 package com.workbook.liuwb.workbook.flutter
 
 import android.os.Bundle
-import android.view.View
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
 import com.workbook.liuwb.workbook.R
-import io.flutter.facade.Flutter
 import kotlinx.android.synthetic.main.activity_test_flutter.*
 
 class TestFlutterActivity : AppCompatActivity() {
@@ -16,23 +12,7 @@ class TestFlutterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_test_flutter)
 
         test_demo.setOnClickListener {
-//            addFlutterFragemnt()
-            addFlutterView()
         }
     }
 
-    private fun addFlutterFragemnt() {
-        val transition: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transition.replace(R.id.test_content, Flutter.createFragment("route1"))
-        transition.commit()
-    }
-
-    private fun addFlutterView() {
-        val flutterView: View = Flutter.createView(this@TestFlutterActivity, lifecycle, "route1")
-        val params = FrameLayout.LayoutParams(600, 800)
-        params.leftMargin = 100
-        params.topMargin = 200
-
-        addContentView(flutterView, params)
-    }
 }
